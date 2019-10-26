b206d3eb95c67ddec2afc0ad60581292c6adae3f
josegonzalo <gonzaloja.1000@gmail.com>
2019-10-26 11:14:06 -0600

    perfil

--- www/js/app.js
+++ www/js/app.js
@@ -81,9 +81,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
+  })
+
+  .state("perfil",{
+    url: "/perfil",
+    templateUrl: "templates/perfil.html"
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise("/perfil");
 
 });
